e0733a40d8c36341fdb53b474efb72b4f0212d12
martinos133 <muha.martin@gmail.com>
2024-04-13 14:15:32 +0200

    adding buttons to photos and creating stravovanie

--- Projekt/martinove podstranky/muha.js
+++ Projekt/martinove podstranky/muha.js
@@ -35,44 +35,56 @@ window.addEventListener('mouseup', (event) => {
   handleSmallScreens()
 
   var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
-
-function openModal(imgSrc, title, description, location, additionalTextId) {
-    document.getElementById('modalImage').src = imgSrc;
-    document.getElementById('modalTitle').innerText = title;
-    document.getElementById('modalDescription').innerText = description;
-    document.getElementById('modalLocation').innerText = location;
-    // Hide all additional texts
-    document.querySelectorAll('.additional-text').forEach(function(text) {
-        text.style.display = 'none';
-    });
-    // Show the specific additional text
-    document.getElementById(additionalTextId).style.display = 'block';
-    modal.style.display = "block";
-}
-
-function closeModal() {
-    modal.style.display = "none";
-    // Hide all additional texts
-    document.querySelectorAll('.additional-text').forEach(function(text) {
-        text.style.display = 'none';
-    });
-}
-
-document.querySelectorAll('.slide').forEach(function(slide) {
-    slide.addEventListener('click', function() {
-        var img = slide.querySelector('img').src;
-        var title = slide.querySelector('h2').innerText;
-        var description = slide.querySelector('p').innerText;
-        var location = slide.querySelector('.difficulty').innerText;
-        var additionalTextId = slide.querySelector('img').getAttribute('data-additional-text');
-        openModal(img, title, description, location, additionalTextId);
-    });
-});
-
-span.onclick = closeModal;
-window.onclick = function(event) {
-    if (event.target == modal) {
-        closeModal();
-    }
-}
+  var span = document.getElementsByClassName("close")[0];
+  var prevBtn = document.getElementById("prevBtn");
+  var nextBtn = document.getElementById("nextBtn");
+  var slides = document.querySelectorAll('.slide');
+  var currentIndex = 0;
+  
+  function openModal(index) {
+      var slide = slides[index];
+      var img = slide.querySelector('img').src;
+      var title = slide.querySelector('h2').innerText;
+      var description = slide.querySelector('p').innerText;
+      var location = slide.querySelector('.difficulty').innerText;
+      var additionalTextId = slide.querySelector('img').getAttribute('data-additional-text');
+  
+      document.getElementById('modalImage').src = img;
+      document.getElementById('modalTitle').innerText = title;
+      document.getElementById('modalDescription').innerText = description;
+      document.getElementById('modalLocation').innerText = location;
+      document.getElementById(additionalTextId).style.display = 'block';
+      modal.style.display = "block";
+  }
+  
+  function closeModal() {
+      modal.style.display = "none";
+      document.querySelectorAll('.additional-text').forEach(function(text) {
+          text.style.display = 'none';
+      });
+  }
+  
+  document.querySelectorAll('.slide').forEach(function(slide, index) {
+      slide.addEventListener('click', function() {
+          currentIndex = index;
+          openModal(currentIndex);
+      });
+  });
+  
+  prevBtn.onclick = function() {
+      currentIndex = (currentIndex > 0) ? currentIndex - 1 : slides.length - 1;
+      openModal(currentIndex);
+  }
+  
+  nextBtn.onclick = function() {
+      currentIndex = (currentIndex < slides.length - 1) ? currentIndex + 1 : 0;
+      openModal(currentIndex);
+  }
+  
+  span.onclick = closeModal;
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          closeModal();
+      }
+  }
+  
